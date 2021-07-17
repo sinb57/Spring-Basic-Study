bbbecefbdbd32d25817cefe6813c4b663f3761f4
Song Inbong <sinb57@naver.com>
2021-07-17 14:01:25 +0900

    BeanDefinition 살펴보기

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class BeanDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
