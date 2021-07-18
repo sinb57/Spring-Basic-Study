098f206affb79876e356eb56add91b5cdcd361cd
Song Inbong <sinb57@naver.com>
2021-07-18 19:53:58 +0900

    탐색할 패키지의 시작 위치 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // basePackages = {"hello.core", "hello.service"},
+        // basePackages : 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다
+        // 만약 지정하지 않으면 @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
-
 
 }
